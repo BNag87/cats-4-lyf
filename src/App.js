09ae5fe3364433187a8c1f625ae2abec8b103864
danny vincent <87720409+1oupd@users.noreply.github.com>
2021-11-01 09:36:29 +0000

    mild updates

--- src/App.js
+++ src/App.js
@@ -1,58 +1,54 @@
 //----------→ REACT IMPORTS ----------→
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 //----------→ CSS IMPORTS ----------→
-import './App.css';
+import "./App.css";
 
 //----------→ COMPONENT IMPORTS ----------→
-import About from './About';
-import Product from './components/Product';
-import Home from './basilsComponents/Home'; 
-import Contacts from './basilsComponents/Contacts';
-import Navbar from './basilsComponents/Navbar';
-import Basket from './basilsComponents/Basket';
+import About from "./About";
+import Product from "./components/Product";
+import Home from "./basilsComponents/Home";
+import Contacts from "./Contacts";
+import Navbar from "./Navbar";
+import Basket from "./basilsComponents/Basket";
+import Footer from "./Footer";
 
-
-  
 //----------→ MAIN FUNCTION ----------→
 const App = () => {
   // ↓ useState used to pass cat elements to basket
-  const [basket, setBasket] = useState([])
+  const [basket, setBasket] = useState([]);
 
   return (
     <Router>
       <div className="App">
         {/* ANYTHING BEFORE "<Navbar/>" WILL BE PERMANENTLY SHOWN/USED */}
-        
 
-        <Navbar/>
-        
-        <div className = "content">
+        <Navbar />
+
+        <div className="content">
           <Switch>
-            
             <Route exact path="/">
               <Home />
-              <Product bket= {basket} sbket = {setBasket}/>
+              <Product bket={basket} sbket={setBasket} />
             </Route>
-            
-            <Route exact path = "/about">
+
+            <Route exact path="/about">
               <About />
             </Route>
 
-            <Route exact path = "/contact">
-              <Contacts/>
-              
+            <Route exact path="/contact">
+              <Contacts />
+
               {/* Basket takes 'props'. messsage displays a <h2> tag. total needs to be calculated, then passed in*/}
-              <Basket basket={basket}/>
+              <Basket basket={basket} />
             </Route>
-            
           </Switch>
         </div>
+        <Footer />
       </div>
     </Router>
   );
-}
+};
 
 export default App;
-
--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,6 @@ import Product from "./components/Product.js";
 function Home() {
   return (
     <>
-      <h2>THIS IS THE HOMEPAGE COMPONENT</h2>
       <Product />
     </>
   );
